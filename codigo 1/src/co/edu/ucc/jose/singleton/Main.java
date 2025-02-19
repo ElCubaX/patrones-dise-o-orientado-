7352afcc6c34cd9d850ddc040a891e96248f49e0
ElCubaX <hunterdead55@gmail.com>
2025-02-18 23:48:42 -0500

    Actualizacion codigo

--- codigo 1/src/co/edu/ucc/jose/singleton/Main.java
+++ codigo 1/src/co/edu/ucc/jose/singleton/Main.java
@@ -5,8 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Banco banco = Banco.getInstance();
         banco.mostrarNombre();
-
-        // Llamar a getIdentidad() al final del programa
+        
         String identidadCodificada = banco.getIdentidad();
         System.out.println("Identidad codificada en Base64: " + identidadCodificada);
     }
