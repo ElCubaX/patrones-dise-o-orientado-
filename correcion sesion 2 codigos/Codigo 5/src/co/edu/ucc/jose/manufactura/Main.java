2aedd889735b49cd9c41bd1ec1b4635f4b1e914a
ElCubaX <hunterdead55@gmail.com>
2025-02-22 20:39:55 -0500

    Actulizacion

--- correcion sesion 2 codigos/Codigo 5/src/co/edu/ucc/jose/manufactura/Main.java
+++ correcion sesion 2 codigos/Codigo 5/src/co/edu/ucc/jose/manufactura/Main.java
@@ -13,16 +13,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Encabezado
         System.out.println("==========================================");
         System.out.println(" Nombre: Jose Emilio Martinez Urgelles");
+        System.out.println(" Historia Usuario: Como gerente de planta, quiero poder calcular el costo total de producción de un lote de acero, para poder planificar el presupuesto.");
         System.out.println(" Universidad: Universidad Cooperativa de Colombia");
         System.out.println(" Curso: Patrones de Diseño Orientado a Objetos");
         System.out.println(" Sector: Manufactura de Acero");
         System.out.println(" Descripción: Cálculo del costo total de producción de un lote de acero.");
         System.out.println("==========================================\n");
 
-        // Entrada de datos
         System.out.print("Ingrese la cantidad de acero (toneladas): ");
         double cantidad = scanner.nextDouble();
 
