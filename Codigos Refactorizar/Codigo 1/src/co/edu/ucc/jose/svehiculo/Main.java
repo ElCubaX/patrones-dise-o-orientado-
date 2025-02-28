80624d8b3d4d17f0656ce94be6c603030af7eef4
ElCubaX <hunterdead55@gmail.com>
2025-02-27 22:45:21 -0500

    Actualizacion

--- Codigos Refactorizar/Codigo 1/src/co/edu/ucc/jose/svehiculo/Main.java
+++ Codigos Refactorizar/Codigo 1/src/co/edu/ucc/jose/svehiculo/Main.java
@@ -1,12 +1,46 @@
-
 package co.edu.ucc.jose.svehiculo;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Base64;
 
 public class Main {
     public static void main(String[] args) {
+        encabezadoPgm();
+        
         Vehiculo v1 = VehiculoFactory.crearVehiculo("electrico");
         Vehiculo v2 = VehiculoFactory.crearVehiculo("gasolina");
 
-        if (v1 != null) v1.conducir();
-        if (v2 != null) v2.conducir();
+        if (v1 != null) v1.arrancar();
+        if (v2 != null) v2.arrancar();
+        
+        System.out.println("\nIdentidad codificada: " + getIdentidad());
+        System.out.println("Patrón implementado: " + getPatron());
+    }
+
+    public static void encabezadoPgm() {
+        String nombre = "Jose Emilio Martinez Urgelles";
+        String campus = "Universidad Cooperativa de Colombia, Campus Cali";
+        String repositorioGit = "https://github.com/habolanos/ucc-estructuras/blob/master/sesion04/ejercicios/1-algoritmo-01/PgmAlgoritmo01.java";
+
+        LocalDateTime ahora = LocalDateTime.now();
+        DateTimeFormatter formateador = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        String fechaHora = ahora.format(formateador);
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("|  Nombre       : " + nombre);
+        System.out.println("|  Campus       : " + campus);
+        System.out.println("|  Fecha y hora : " + fechaHora);
+        System.out.println("|  Repositorio Git : " + repositorioGit);
+        System.out.println("--------------------------------------------------\n");
+    }
+
+    public static String getIdentidad() {
+        String nombre = "Jose Emilio Martinez Urgelles";
+        return Base64.getEncoder().encodeToString(nombre.getBytes());
+    }
+
+    public static String getPatron() {
+        return "Factory Method";
     }
 }
