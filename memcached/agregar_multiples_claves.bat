ec1f1d6522815464b9b94d804c27d5593183628a
ElCubaX <hunterdead55@gmail.com>
2025-04-11 22:54:32 -0500

    modelos de coherencia

---
+++ memcached/agregar_multiples_claves.bat
@@ -0,0 +1,3 @@
+echo set producto1 0 900 8^
+producto1^
+| ncat localhost 11211
---
+++ memcached/get_claves.bat
@@ -0,0 +1,4 @@
+@echo off
+echo get producto1 | ncat localhost 11211
+echo get producto2 | ncat localhost 11211
+echo get producto3 | ncat localhost 11211
